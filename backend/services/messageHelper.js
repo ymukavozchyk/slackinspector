00058c1c154b40bed41244e7e58d1cf324231a06
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-10-17 15:37:15 -0500

    Adjusted message helper

--- backend/services/messageHelper.js
+++ backend/services/messageHelper.js
@@ -3,9 +3,10 @@
 var mask = ['.', ';', '!', '?'];
 
 module.exports = {
-    getSuffix: function(message){
-        if(message !== ''){
-            if(mask.includes(message.slice(-1))){
+    getSuffix: function (message) {
+        if (message !== '') {
+            var lastCharacter = message.slice(-1);
+            if (mask.indexOf(lastCharacter) >= 0) {
                 return '';
             }
             return '.';
