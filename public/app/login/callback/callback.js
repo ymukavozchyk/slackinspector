c87477743cb294a318a2321b75e009d77c4ccd9b
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-11-05 22:12:25 -0500

    Added callback error and redirect

--- public/app/login/callback/callback.js
+++ public/app/login/callback/callback.js
@@ -1,26 +1,48 @@
-(function () {
+(function() {
     'use strict';
 
     angular
         .module('app.login.callback')
         .controller('CallbackController', CallbackController);
 
-    CallbackController.$inject = ['$state', '$stateParams', 'CredentialService', 'ApiService'];
-    function CallbackController($state, $stateParams, CredentialService, ApiService) {
+    CallbackController.$inject = ['$state', '$stateParams', '$interval', 'CredentialService', 'ApiService'];
+    function CallbackController($state, $stateParams, $interval, CredentialService, ApiService) {
         var vm = this;
+
+        var redirectDelayInSeconds = 10;
+        var redirectDelay = redirectDelayInSeconds * 1000;
+        var interval;
+
+        vm.hideLoader = false;
+        vm.errorMessage = '';
+        vm.counter = redirectDelayInSeconds;
 
         activate();
 
         function activate() {
-            ApiService.login($stateParams.code)
-                .then(function (res) {
+            ApiService.login('$stateParams.code')
+                .then(function(res) {
                     CredentialService.setToken(res.data.encrypted_token);
                     CredentialService.resetState();
                     $state.go('main.step1');
                 },
-                function (e) {
-                    alert(e.data.error_type + ' : ' + e.data.error);
+                function(e) {
+                    vm.errorMessage = e.data.error_type + ' : ' + e.data.error;
+                    vm.hideLoader = true;
+
+                    interval = $interval(countInterval, 1000);
                 });
+        }
+
+        function countInterval(){
+            if(vm.counter > 1){
+                vm.counter--;
+            }
+            else{
+                $interval.cancel(interval);
+                interval = undefined;
+                $state.go('login');
+            }
         }
     }
 })();
