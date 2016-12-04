dd519e745b8103a277ae8468908b2089fdd96153
Yauheni Mukavozchyk <ymukavozchyk@luc.edu>
2016-12-04 14:22:10 -0600

    Added CSS bar chart for usage statistics

--- public/app/main/usage/usage.js
+++ public/app/main/usage/usage.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -14,24 +14,68 @@
         vm.errorType = '';
         vm.errorMessage = '';
 
+        vm.languageTones = [];
+        vm.emotionTones = [];
+        vm.socialTones = [];
+
         getUsageStatistics();
 
         function getUsageStatistics() {
             ApiService.getUsageStatistics()
-                .then(function(res) {
-                    vm.hideLoader = true;
-                    console.log(res.data);
+                .then(function (res) {
+                    var processingErrorFlag = false;
+
+                    res.data.result.forEach(function (toneSection) {
+                        toneSection.tones.forEach(function (tone) {
+                            var score = tone.average_value;
+                            if (score > 1) {
+                                score = 1;
+                            }
+                            score = score.toFixed(2);
+
+                            var toneObj = {
+                                name: tone.tone_name,
+                                score: score,
+                                style: { width: (score * 100) + '%' }
+                            };
+
+                            switch (toneSection._id.category_id) {
+                                case 'language_tone':
+                                    vm.languageTones.push(toneObj);
+                                    break;
+                                case 'emotion_tone':
+                                    vm.emotionTones.push(toneObj);
+                                    break;
+                                case 'social_tone':
+                                    vm.socialTones.push(toneObj);
+                                    break;
+                                default:
+                                    processingErrorFlag = true;
+                            }
+                        });
+                    });
+
+                    if (processingErrorFlag === true) {
+                        formErrorMessage('Client', 'Was not able to parse usage statistics');
+                    }
+                    else {
+                        vm.hideLoader = true;
+                    }
                 },
-                function(e) {
-                    vm.errorType = e.data.error_type;
-                    vm.errorMessage = e.data.error;
-                    vm.hideLoader = true;
-                    vm.showError = true;
-                    vm.showErrorDialog();
+                function (e) {
+                    formErrorMessage(e.data.error_type, e.data.error);
                 });
         }
 
-        vm.showErrorDialog = function() {
+        function formErrorMessage(errorType, errorMessage) {
+            vm.errorType = errorType;
+            vm.errorMessage = errorMessage;
+            vm.hideLoader = true;
+            vm.showError = true;
+            vm.showErrorDialog();
+        }
+
+        vm.showErrorDialog = function () {
             var errorTitle = 'Error from ' + vm.errorType;
             $mdDialog.show(
                 $mdDialog.alert()
@@ -44,7 +88,7 @@
             );
         };
 
-        vm.tryAgain = function() {
+        vm.tryAgain = function () {
             vm.hideLoader = false;
             vm.showError = false;
             vm.errorType = '';
